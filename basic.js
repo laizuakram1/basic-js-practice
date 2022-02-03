5be6acb929fd5af17aac48e313976838afdb3f7e
laizuakram1 <laizuakram98@gmail.com>
2022-02-04 01:49:24 +0600

    problem solving-2

---
+++ basic.js
@@ -0,0 +1,68 @@
+
+// let myName = "laizu Akram";
+// let myNumbers = 01770850305;
+// let myBoolean = true;
+
+
+// let num = 28;
+// let num2 = 16;
+
+// let tempN = num;
+// num = num2;
+// num2 = tempN;
+
+// using && and || 
+
+// function myFunction(number){
+//     if(number % 2 == 0 || number % 4 == 0){
+//         return'this is my parameter';
+//     }
+//     else{
+//         return 'this not my parameter';
+//     }
+// }
+// let myResult = myFunction(15);
+// console.log(myResult);
+
+
+
+// find 1 to 19 even numbers by using while loop
+// let i = 1;
+// while( i <= 19){
+//     if(i % 2 != 0){
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+// let myArrays = [50, 26, 82, 45, 90, 48, 85, 87, 37];
+
+
+// let count = 0;
+// for(let element of myArrays){
+//     if(element > 80 ){
+//       console.log(element)
+//     }
+   
+    
+// }
+
+
+
+// function myNumebrs(num1, num2, num3){
+//     let totalNumbers = 0
+//     totalNumbers = num1 * num2 * num3;
+//     return totalNumbers;
+// }
+// let result = myNumebrs(23, 30, 12);
+// console.log(result);
+
+// let guiter = {
+//     brand: 'yamaha',
+//     price: 18500,
+//     color: 'black'
+// };
+// guiter.price = 14500;
+// console.log(guiter);
+
+
---
+++ problem-solving.js
@@ -0,0 +1,18 @@
+// convert feet to inches
+
+function feetToInch(feet){
+    let calculateFeet = feet * 12;
+    return calculateFeet;
+}
+let totalInches = feetToInch(5);
+console.log(totalInches);
+
+
+// convert centimeter to meter 
+
+function centimeterToMeter(centrimeter){
+    let calculateMeter = centrimeter * 0.01;
+    return calculateMeter;
+}
+let totalMeter = centimeterToMeter(110);
+console.log(totalMeter);
